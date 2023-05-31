a5235def1301b0cf9f1edef4fdcdba1bc2aa297c
88joonyc <joonyc88@gmail.com>
2023-05-30 23:35:31 -0400

    validation

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { handleValidationErrors } = require('../../utils/validation');
+
 
 const router = express.Router();
+
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors,
+];
 
 // Sign up
 router.post(
     '/',
+    validateSignup,
     asyncHandler(async (req, res) => {
       const { email, password, username } = req.body;
       const user = await User.signup({ email, username, password });
